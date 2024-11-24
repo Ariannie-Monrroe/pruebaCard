ee8bda52629c653bcd8f2cada92263590e46efd2
LZamoraVillablanca <lukaszamora.821@gmail.com>
2024-11-24 19:41:30 -0300

    borradoComentarios

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,19 +1,20 @@
-// variables
-const precioInicial = document.querySelector("#precioInicial")
-const cantidad = document.querySelector("#cantidad")
-const total = document.querySelector("#total")
+const precioInicial = document.querySelector("#precioInicial");
+const cantidad = document.querySelector("#cantidad");
+const total = document.querySelector("#total");
 
-// botones
-const menos = document.querySelector("#menos")
-const mas = document.querySelector("#mas")
+const menos = document.querySelector("#menos");
+const mas = document.querySelector("#mas");
 
-// funcionalidad de botones
-mas.addEventListener('click', () => {
-    cantidad.innerHTML++
-    total.innerHTML= (precioInicial.innerHTML*cantidad.innerHTML).toLocaleString()
-})
+mas.addEventListener("click", () => {
+  cantidad.innerHTML++;
+  total.innerHTML = (
+    precioInicial.innerHTML * cantidad.innerHTML
+  ).toLocaleString();
+});
 
-menos.addEventListener('click', () => {
-    cantidad.innerHTML--
-    total.innerHTML= (precioInicial.innerHTML*cantidad.innerHTML).toLocaleString()
-})
+menos.addEventListener("click", () => {
+  cantidad.innerHTML--;
+  total.innerHTML = (
+    precioInicial.innerHTML * cantidad.innerHTML
+  ).toLocaleString();
+});
